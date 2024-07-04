3e837d72b78735baac560e8f54f53c14c6526ac4
mcfi3rce <aomcpherson@gmail.com>
2024-07-04 17:35:58 -0400

    fix: pt01 passing

--- day07.go
+++ day07.go
@@ -14,13 +14,13 @@ type PokerHand struct {
 	Value       int
 	CardCount   int
 	HandType    rune
-	HighCard    int
 	IsFullHouse bool
+	IsTwoPair   bool
 }
 
 var cardValues = map[rune]int{
 	'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
-	'T': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14,
+	'T': 10, 'J': 1, 'Q': 12, 'K': 13, 'A': 14,
 }
 
 func extractHandValues(key string) PokerHand {
@@ -54,12 +54,14 @@ func extractHandValues(key string) PokerHand {
 		}
 	}
 
-	// Find the high card
-	var remaining = strings.ReplaceAll(hand.Key, string(hand.HandType), "")
-	for i := 0; i < len(remaining); i++ {
-		if cardValues[rune(remaining[i])] > hand.HighCard {
-			hand.HighCard = cardValues[rune(remaining[i])]
+	pairs := 0
+	for _, r := range cardCounts {
+		if r == 2 {
+			pairs++
 		}
+	}
+	if pairs == 2 {
+		hand.IsTwoPair = true
 	}
 
 	return hand
@@ -102,30 +104,29 @@ func day07pt1() {
 				}
 			}
 
-			// Return the higher of the two card counts
-			if cardValues[hands[i].HandType] > cardValues[hands[j].HandType] {
-				fmt.Println("Hand:", hands[i], "Higher than Hand:", hands[j])
-				fmt.Println("Card Values:", cardValues[hands[i].HandType], "VS", cardValues[hands[j].HandType])
-				return false
+			if hands[i].CardCount == 2 {
+				// FullHouse has higher priority
+				if hands[i].IsTwoPair && !hands[j].IsTwoPair {
+					return false
+				}
+				if !hands[i].IsTwoPair && hands[j].IsTwoPair {
+					return true
+				}
 			}
 
-			if cardValues[hands[i].HandType] < cardValues[hands[j].HandType] {
-				fmt.Println("Hand:", hands[j], "Lower than Hand:", hands[i])
-				fmt.Println("Card Values:", cardValues[hands[i].HandType], "VS", cardValues[hands[j].HandType])
-				return true
-			}
-
-			fmt.Println("USING HIGH CARD", hands[i], "VS", hands[j])
-			if hands[i].HighCard < hands[j].HighCard {
-				fmt.Println("Hand:", hands[i], "Lower than Hand:", hands[j])
-				fmt.Println("High Cards:", hands[i].HighCard, "VS", hands[j].HighCard)
-				return true
-			}
-
-			if hands[i].HighCard > hands[j].HighCard {
-				fmt.Println("Hand:", hands[i], "Higher than Hand:", hands[j])
-				fmt.Println("High Cards:", hands[i].HighCard, "VS", hands[j].HighCard)
-				return false
+			for k := 0; k < len(hands[i].Key); k++ {
+				if cardValues[rune(hands[i].Key[k])] > cardValues[rune(hands[j].Key[k])] {
+					//fmt.Println("Hand a: ", string(hands[i].Key[k]), " > Hand b: ", string(hands[j].Key[k]))
+					//fmt.Println("Card Number: ", k+1, "Card Value: ", cardValues[rune(hands[i].Key[k])], "Card Value: ", cardValues[rune(hands[j].Key[k])])
+					//fmt.Println("Hand:", hands[i], "Hand:", hands[j])
+					return false
+				}
+				if cardValues[rune(hands[i].Key[k])] < cardValues[rune(hands[j].Key[k])] {
+					//fmt.Println("Hand a: ", string(hands[i].Key[k]), " < Hand b: ", string(hands[j].Key[k]))
+					//fmt.Println("Card Number: ", k+1, "Card Value: ", cardValues[rune(hands[i].Key[k])], "Card Value: ", cardValues[rune(hands[j].Key[k])])
+					//fmt.Println("Hand:", hands[i], "Hand:", hands[j])
+					return true
+				}
 			}
 
 		}
@@ -148,6 +149,71 @@ func day07pt2() {
 		log.Fatal(err)
 	}
 	text := strings.Split(string(file), "\n")
-	fmt.Println(text)
+
+	var hands []PokerHand
+
+	// Start with setting the key to the hand
+	// Then sort it in order of card counts then card values
+
+	for _, line := range text {
+		keyValue := strings.Fields(line)
+		hand := extractHandValues(keyValue[0])
+		hand.Value, _ = strconv.Atoi(keyValue[1])
+		hands = append(hands, hand)
+	}
+
+	// Sort the hands by card count then high card
+	// Assuming hands is a slice of PokerHand
+	// Note this is sorted lowest to highest so if it's higher rank return false
+	sort.Slice(hands, func(i, j int) bool {
+		// If neither or both are FullHouse, sort by CardCount, then by HighCard
+		if hands[i].CardCount == hands[j].CardCount {
+
+			if hands[i].CardCount == 3 {
+				// FullHouse has higher priority
+				if hands[i].IsFullHouse && !hands[j].IsFullHouse {
+					return false
+				}
+				if !hands[i].IsFullHouse && hands[j].IsFullHouse {
+					return true
+				}
+			}
+
+			if hands[i].CardCount == 2 {
+				// FullHouse has higher priority
+				if hands[i].IsTwoPair && !hands[j].IsTwoPair {
+					return false
+				}
+				if !hands[i].IsTwoPair && hands[j].IsTwoPair {
+					return true
+				}
+			}
+
+			for k := 0; k < len(hands[i].Key); k++ {
+				if cardValues[rune(hands[i].Key[k])] > cardValues[rune(hands[j].Key[k])] {
+					//fmt.Println("Hand a: ", string(hands[i].Key[k]), " > Hand b: ", string(hands[j].Key[k]))
+					//fmt.Println("Card Number: ", k+1, "Card Value: ", cardValues[rune(hands[i].Key[k])], "Card Value: ", cardValues[rune(hands[j].Key[k])])
+					//fmt.Println("Hand:", hands[i], "Hand:", hands[j])
+					return false
+				}
+				if cardValues[rune(hands[i].Key[k])] < cardValues[rune(hands[j].Key[k])] {
+					//fmt.Println("Hand a: ", string(hands[i].Key[k]), " < Hand b: ", string(hands[j].Key[k]))
+					//fmt.Println("Card Number: ", k+1, "Card Value: ", cardValues[rune(hands[i].Key[k])], "Card Value: ", cardValues[rune(hands[j].Key[k])])
+					//fmt.Println("Hand:", hands[i], "Hand:", hands[j])
+					return true
+				}
+			}
+
+		}
+		return hands[i].CardCount < hands[j].CardCount || hands[j].CardCount > hands[i].CardCount
+	})
+
+	total := 0
+	for i, hand := range hands {
+		total += hand.Value * (i + 1)
+		fmt.Println("Hand:", hand, "Value:", hand.Value*(i+1), "Rank: ", i+1, "Total:", total)
+	}
+
+	fmt.Println("Total:", total)
 
 }
